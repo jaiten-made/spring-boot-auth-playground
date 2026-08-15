2bb96ed1be7c45c6985f6017d0d9c528a8ce6864
Jaiten Made <jaiten-made@users.noreply.github.com>
2026-08-15 16:12:00 +1000

    refactor: rename application class and project to AuthPlaygroundApplication

--- src/main/java/com/springboot/playground/auth/AuthPlaygroundApplication.java
+++ src/main/java/com/springboot/playground/auth/AuthPlaygroundApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAuthApplication {
+public class AuthPlaygroundApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAuthApplication.class, args);
+		SpringApplication.run(AuthPlaygroundApplication.class, args);
 	}
 
 }
